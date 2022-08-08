659babeafdc388303355332eb665c4fb37e85563
Matheus Porto <matheus.porto@picpay.com>
2022-08-07 23:46:17 -0300

    ajustando input e leitura de arquivo fonta

--- motherLang/src/MainClass.java
+++ motherLang/src/MainClass.java
@@ -4,10 +4,31 @@ import java.lang.Math;
 public class MainClass {
     public static void main(String args[]) {
         Scanner _key = new Scanner(System.in);
-        Boolean  b;
-        String  t;
+        double  a;
+        String  c;
+        Boolean  t;
         double  x;
-        double  y;
-        String  z;
+        double  zero0;
+        t = true;
+        a = -1;
+        zero0 = 0;
+        x = 3+2*20/4;
+        System.out.println(x);
+        if (t) {
+            c = "deu bom";            
+} else {
+            c = "deu ruim";            
+}
+
+        System.out.println(c);
+        if (a>=zero0) {
+            c = "variavel A maior ou igual que zero";            
+} else {
+            c = "variavel A menor que zero";            
+}
+
+        System.out.println(c);
+        System.out.println(Math.pow(3.0,2));
+
     }
 }
--- motherLang/src/br/com/ufabc/motherLanguage/main/MainClass.java
+++ motherLang/src/br/com/ufabc/motherLanguage/main/MainClass.java
@@ -6,10 +6,22 @@ import br.com.ufabc.motherLanguage.parser.MotherLangParser;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 
+import static br.com.ufabc.motherLanguage.main.MainClass.MotherLangInputSource.*;
+
 public class MainClass {
+    enum MotherLangInputSource {
+        INPUT_ENTREGA("input.mother"),
+        INPUT_TESTANDO_ERROS("input_teste_erros.mother");
+
+        public final String ambiente;
+
+        MotherLangInputSource(String ambiente) {
+            this.ambiente = ambiente;
+        }
+    }
     public static void main(String[] args) {
         try {
-            MotherLangLexer lexer = new MotherLangLexer(CharStreams.fromFileName("input.mother"));
+            MotherLangLexer lexer = new MotherLangLexer(CharStreams.fromFileName(INPUT_TESTANDO_ERROS.ambiente));
             CommonTokenStream tokenStream = new CommonTokenStream(lexer);
             MotherLangParser parser = new MotherLangParser(tokenStream);
 
